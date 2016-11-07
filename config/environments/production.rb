07f82aa045871016c8013296aca3666adc8fda64
manoj <manoj.prithvee@gmail.com>
2016-11-08 01:24:29 +0530

    after

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Developertool::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
 
   # Generate digests for assets URLs.
