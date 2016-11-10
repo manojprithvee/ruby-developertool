4e4f9eb3b1d9429c3ddb025556cc9732b3e4316e
manoj <manoj.prithvee@gmail.com>
2016-11-11 02:29:34 +0530

    after

--- app/helpers/tools_helper.rb
+++ app/helpers/tools_helper.rb
@@ -2,11 +2,15 @@ require 'faraday'
 require 'json'
 module ToolsHelper
 def getipdetails(ip)
+begin
 abc=JSON.parse(Faraday.get("http://ip-api.com/json/"+ip).body)
 puts 
 if !abc.nil? and !abc["status"].nil? and abc["status"]=="success" and !abc["isp"].nil?
 return abc["isp"]
 else 
+return nil
+end
+rescue
 return nil
 end
 end
