c86cb68fbbc3036ae1e59cdb263490b1a40dd125
manoj <manoj.prithvee@gmail.com>
2016-12-11 17:52:24 +0530

    after cache

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -6,38 +6,52 @@ class ToolsController < ApplicationController
   end
 
   def differ_text
+  expires_in 1.day, public: true, must_revalidate: true
   end
 
   def password_gen
+  expires_in 1.day, public: true, must_revalidate: true
   end
 
   def hash_generator
+  expires_in 1.day, public: true, must_revalidate: true
   end
 
   def url_encode
+  expires_in 1.day, public: true, must_revalidate: true
   end
 
   def string_search
+  expires_in 1.day, public: true, must_revalidate: true
   end
 
   def code_format
+  expires_in 1.day, public: true, must_revalidate: true
   end
 
   def about
+  expires_in 1.day, public: true, must_revalidate: true
   end
 
   def contact
+  expires_in 1.day, public: true, must_revalidate: true
   end
   def index
+  expires_in 1.day, public: true, must_revalidate: true
   end
   def privacypolicy
+  expires_in 1.month, public: true, must_revalidate: true
   end
   def base64encode
+  expires_in 1.day, public: true, must_revalidate: true
   end
   def ipaddress
+  expires_in 1.day, public: true, must_revalidate: true
   end
   def xpath
+  expires_in 1.day, public: true, must_revalidate: true
   end
   def sslverify
+  expires_in 1.day, public: true, must_revalidate: true
   end
 end
