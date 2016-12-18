40cb9865dca1f6a3d61e72fad4b09775cb0450b0
manoj <manoj.prithvee@gmail.com>
2016-12-18 18:34:31 +0530

    after adding database rds

--- app/assets/javascripts/hash.js
+++ app/assets/javascripts/hash.js
@@ -1,8 +1,12 @@
 $(document).ready(function () {
 $('#text').on("input",function () {
+    $("#output").html("<div id='wrapper' class='jumbotron'><div class='profile-main-loader'><div class='loader'><svg class='circular-loader'viewBox='25 25 50 50' ><circle class='loader-path' cx='50' cy='50' r='20' fill='none' stroke='#70c542' stroke-width='2' /></svg></div></div></div></div>");
     $.ajax({url: "https://developertoolsphp.herokuapp.com/hash.php?string="+$(this).val(), success: function(result){
         $("#output").html(result)
+        
     }
-});
+}).fail(function() {
+    $("#output").html("<div class='jumbotron'><strong>There is a Problem</strong></div>");
+  });
 });
 });
--- app/assets/javascripts/sslverify.js
+++ app/assets/javascripts/sslverify.js
@@ -5,7 +5,7 @@ $("#Submit").on("click",function(){
     urls=$("#url").val()
     if (urls!=""){
         $("#Submit").html("Loading...")
-        $("#output").html("<p>Process has started it might take upto  5 minutes for it to be done..")
+        $("#output").html("<div id='wrapper' class='jumbotron'><div class='profile-main-loader'><div class='loader'><svg class='circular-loader'viewBox='25 25 50 50' ><circle class='loader-path' cx='50' cy='50' r='20' fill='none' stroke='#70c542' stroke-width='2' /></svg></div></div></div></div>");
         $.ajax({url: "/api/sslverify?host="+urls, success: function(result){
             console.log(result)
             $("#output").html(result)
